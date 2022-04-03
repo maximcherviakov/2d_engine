dd22a7ba825346cafdacc586a76c3971526682f8
Maxim <cherviakovmaxim@gmail.com>
2022-04-03 17:20:16 +0200

    OpenGL initialization

--- src/main.cpp
+++ src/main.cpp
@@ -3,34 +3,50 @@
 
 #include <iostream>
 
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
+void glfwWindowSizeCallback(GLFWwindow* window, int windowSizeX, int windowSizeY);
+void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
 int main(void)
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
+        std::cerr << "Initialization failed!" << std::endl;
         return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Engine", nullptr, nullptr);
     if (!window)
     {
+        std::cerr << "Window was not created!" << std::endl;
         glfwTerminate();
         return -1;
     }
 	
+    glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+    glfwSetKeyCallback(window, glfwKeyCallback);
+
 	/* Make the window's context current */
     glfwMakeContextCurrent(window);
 	
 	if (!gladLoadGL())
 	{
-		std::cout << "GLAD initialization failed" << std::endl;
+		std::cerr << "GLAD initialization failed" << std::endl;
 		return -1;
 	}
 	
-	std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
-
-    glClearColor(0, 1, 0, 1);
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+	
+    glClearColor(0, 1, 1, 1);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -43,5 +59,17 @@ int main(void)
 
         /* Poll for and process events */
         glfwPollEvents();
+    }
+}
+
+void glfwWindowSizeCallback(GLFWwindow* window, int windowSizeX, int windowSizeY) {
+    g_windowSizeX = windowSizeX;
+    g_windowSizeY = windowSizeY;
+    glViewport(0, 0, 50, windowSizeY/2);
+}
+
+void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
     }
 }
